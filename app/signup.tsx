9e0475e3d78ea2cc9f6bdbb800da9783319c9c33
Learn2manage <learn2manage@gmail.com>
2024-04-08 20:46:59 +0800

    edit signup.tsx to add 2 Text tags and a mobile number TextInput.

--- app/signup.tsx
+++ app/signup.tsx
@@ -1,12 +1,58 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native';
+import React, { useState } from 'react';
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
+    const [countryCode, setCountryCode] = useState('+852');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    //const completePhoneNumber = `${countryCode}${phoneNumber}`;
+    const onSignup = async () => {};
+
     return (
-        <View>
-            <Text>Signup</Text>
+        <View style={defaultStyles.container}>
+            <Text style={defaultStyles.header}>Let's get started</Text>
+            <Text style={defaultStyles.descriptionText}>
+                Enter your phone number. We will send you a confirmation code.
+            </Text>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Mobile Number"
+                    placeholderTextColor={Colors.gray}
+                    keyboardType="numeric"
+                    value={phoneNumber}
+                    onChangeText={(text) => setPhoneNumber(text)}></TextInput>
+            </View>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        marginVertical: 40,
+        flexDirection: 'row',
+    },
+    input: {
+        backgroundColor: Colors.lightGray,
+        padding: 20,
+        borderRadius: 16,
+        fontSize: 20,
+        marginRight: 10,
+        width: 240,
+    },
+    enabled: {
+        backgroundColor: Colors.primary,
+    },
+    disabled: {
+        backgroundColor: Colors.primaryMuted,
+    },
+});
 
 export default Page;
