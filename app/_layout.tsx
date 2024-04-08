4346a4c6785722572146cf76dd7735db3c8146f7
Learn2manage <learn2manage@gmail.com>
2024-04-08 23:01:38 +0800

    Further editing signup.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,11 @@
+import Colors from '@/constants/Colors';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
 
 export {
     // Catch any errors thrown by the Layout component.
@@ -45,6 +48,24 @@ function RootLayoutNav() {
     return (
         <Stack>
             <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+                name="signup"
+                options={{
+                    title: '',
+                    headerBackTitle: '',
+                    headerShadowVisible: false,
+                    headerStyle: { backgroundColor: Colors.background },
+                    headerLeft: () => (
+                        <TouchableOpacity>
+                            <Ionicons
+                                name="arrow-back"
+                                size={30}
+                                color={Colors.dark}
+                            />
+                        </TouchableOpacity>
+                    ),
+                }}
+            />
         </Stack>
     );
 }
--- app/signup.tsx
+++ app/signup.tsx
@@ -8,11 +8,12 @@ import {
 import React, { useState } from 'react';
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
+import { Link } from 'expo-router';
 
 const Page = () => {
     const [countryCode, setCountryCode] = useState('+852');
     const [phoneNumber, setPhoneNumber] = useState('');
-    //const completePhoneNumber = `${countryCode}${phoneNumber}`;
+    const completePhoneNumber = `${countryCode}${phoneNumber}`;
     const onSignup = async () => {};
 
     return (
@@ -24,12 +25,38 @@ const Page = () => {
             <View style={styles.inputContainer}>
                 <TextInput
                     style={styles.input}
+                    placeholder="Country Code"
+                    placeholderTextColor={Colors.gray}
+                    keyboardType="numeric"
+                    value={countryCode}
+                    onChangeText={setCountryCode}
+                />
+                <TextInput
+                    style={[styles.input, { flex: 1 }]}
                     placeholder="Mobile Number"
                     placeholderTextColor={Colors.gray}
                     keyboardType="numeric"
                     value={phoneNumber}
-                    onChangeText={(text) => setPhoneNumber(text)}></TextInput>
+                    onChangeText={setPhoneNumber}
+                />
             </View>
+            <Link href={'/login'} replace asChild>
+                <TouchableOpacity>
+                    <Text style={defaultStyles.textLink}>
+                        Already have an account? Log in
+                    </Text>
+                </TouchableOpacity>
+            </Link>
+            <View style={{ flex: 1 }} />
+            <TouchableOpacity
+                style={[
+                    defaultStyles.pillButton,
+                    phoneNumber !== '' ? styles.enabled : styles.disabled,
+                    { marginBottom: 20 },
+                ]}
+                onPress={onSignup}>
+                <Text style={defaultStyles.buttonText}>Sign up</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -45,7 +72,6 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         fontSize: 20,
         marginRight: 10,
-        width: 240,
     },
     enabled: {
         backgroundColor: Colors.primary,
