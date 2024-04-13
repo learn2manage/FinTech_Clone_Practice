776fd809976764dddbe4b3a149c3f3f281e7a8e6
Learn2manage <learn2manage@gmail.com>
2024-04-13 10:12:15 +0800

    fixed _layout.tsx path error. Proceeded further in the process.

--- app/help.tsx
+++ app/help.tsx
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
+import { defaultStyles } from '@/constants/Styles';
+import Colors from '@/constants/Colors';
 
 const Page = () => {
     return (
         <View>
             <Text>Help</Text>
+            <Link
+                href={'/test/testing'}
+                style={[
+                    defaultStyles.pillButton,
+                    { flex: 1, backgroundColor: Colors.dark },
+                ]}
+                asChild>
+                <TouchableOpacity>
+                    <Text
+                        style={{
+                            color: 'white',
+                            fontSize: 22,
+                            fontWeight: '500',
+                        }}>
+                        Log in
+                    </Text>
+                </TouchableOpacity>
+            </Link>
         </View>
     );
 };
--- app/test/testing.tsx
+++ app/test/testing.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-const Page = () => {
+const testing = () => {
     return (
         <View>
-            <Text>Authenticated Home</Text>
+            <Text>testing</Text>
         </View>
     );
 };
 
-export default Page;
+export default testing;
