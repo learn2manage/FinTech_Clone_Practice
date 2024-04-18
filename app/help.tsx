d6cca35b562a37761f51758f3b5992d193af743e
Learn2Manage <learn2manage@gmail.com>
2024-04-18 16:47:34 +0800

    Fixed the subfolder error that appears again.

--- app/help.tsx
+++ app/help.tsx
@@ -1,33 +1,32 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
 import { Link } from 'expo-router';
-import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
 
 const Page = () => {
-    return (
-        <View>
-            <Text>Help</Text>
-            <Link
-                href={'/test/testing'}
-                style={[
-                    defaultStyles.pillButton,
-                    { flex: 1, backgroundColor: Colors.dark },
-                ]}
-                asChild>
-                <TouchableOpacity>
-                    <Text
-                        style={{
-                            color: 'white',
-                            fontSize: 22,
-                            fontWeight: '500',
-                        }}>
-                        Log in
-                    </Text>
-                </TouchableOpacity>
-            </Link>
-        </View>
-    );
+  return (
+    <View>
+      <Text>Help</Text>
+      <Link
+        href={'/test/testing'}
+        style={[
+          defaultStyles.pillButton,
+          { flex: 1, backgroundColor: Colors.dark },
+        ]}
+        asChild>
+        <TouchableOpacity>
+          <Text
+            style={{
+              color: 'white',
+              fontSize: 22,
+              fontWeight: '500',
+            }}>
+            Log in
+          </Text>
+        </TouchableOpacity>
+      </Link>
+    </View>
+  );
 };
 
 export default Page;
