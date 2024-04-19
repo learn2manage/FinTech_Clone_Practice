d5f2e3a1f521e594646b6d7fee1b1ed5f0a3acda
Learn2manage <learn2manage@gmail.com>
2024-04-19 22:45:47 +0800

    Display transactions in home tab and show the widgets after the transactions.

--- app/(authenticated)/(tabs)/home.tsx
+++ app/(authenticated)/(tabs)/home.tsx
@@ -4,76 +4,131 @@ import Colors from '@/constants/Colors';
 import RoundBtn from '@/components/RoundBtn';
 import Dropdown from '@/components/Dropdown';
 import { useBalanceStore } from '@/store/balance-storage';
+import { defaultStyles } from '@/constants/Styles';
+import { Ionicons } from '@expo/vector-icons';
+import { center } from '@shopify/react-native-skia';
+import WidgetList from '@/components/SortableList/WidgetList';
 
 const Page = () => {
-  const { balance, runTransaction, transactions, clearTransactions } =
-    useBalanceStore();
-  const onAddMoney = () => {
-    runTransaction({
-      id: Math.random().toString(),
-      amount: 100,
-      date: new Date(),
-      title: 'Add money',
-    });
-  };
+    const { balance, runTransaction, transactions, clearTransactions } =
+        useBalanceStore();
 
-  return (
-    <ScrollView style={{ backgroundColor: '#58d894' }}>
-      <View style={styles.account}>
-        <View style={styles.row}>
-          <Text style={styles.currency}>$</Text>
-          <Text style={styles.balance}>{balance()}</Text>
-        </View>
-      </View>
-      <View style={styles.actionRow}>
-        <RoundBtn icon={'add'} text={'Add Money'} onPress={onAddMoney} />
-        <RoundBtn icon={'refresh'} text={'Exchange'} onPress={onAddMoney} />
-        <RoundBtn icon={'list'} text={'Details'} onPress={onAddMoney} />
-        <Dropdown />
-      </View>
-    </ScrollView>
-  );
+    const onAddMoney = () => {
+        runTransaction({
+            id: Math.random().toString(),
+            amount:
+                Math.floor(Math.random() * 1000) *
+                (Math.random() > 0.5 ? 1 : -1),
+            date: new Date(),
+            title: 'Add money',
+        });
+    };
+
+    return (
+        <ScrollView
+            style={{ backgroundColor: '#58d894' }}
+            contentContainerStyle={{
+                paddingTop: 0,
+            }}>
+            <View style={styles.account}>
+                <View style={styles.row}>
+                    <Text style={styles.currency}>$</Text>
+                    <Text style={styles.balance}>{balance()}</Text>
+                </View>
+            </View>
+            <View style={styles.actionRow}>
+                <RoundBtn
+                    icon={'add'}
+                    text={'Add Money'}
+                    onPress={onAddMoney}
+                />
+                <RoundBtn
+                    icon={'refresh'}
+                    text={'Exchange'}
+                    onPress={clearTransactions}
+                />
+                <RoundBtn icon={'list'} text={'Details'} />
+                <Dropdown />
+            </View>
+            <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+            <View style={styles.transactions}>
+                {transactions.length === 0 && (
+                    <Text style={{ padding: 14, color: Colors.gray }}>
+                        No Transactions yet
+                    </Text>
+                )}
+                {transactions.reverse().map((transaction) => (
+                    <View
+                        key={transaction.id}
+                        style={{
+                            flexDirection: 'row',
+                            alignItems: 'center',
+                            gap: 16,
+                        }}>
+                        <View style={styles.circle}>
+                            <Ionicons
+                                name={transaction.amount > 0 ? 'add' : 'remove'}
+                                size={24}
+                                color={Colors.primary}
+                            />
+                        </View>
+                        <View style={{ flex: 1 }}>
+                            <Text style={{ fontWeight: '400' }}>
+                                {transaction.title}
+                            </Text>
+                            <Text style={{ color: Colors.gray, fontSize: 12 }}>
+                                {transaction.date.toLocaleDateString()}
+                            </Text>
+                        </View>
+                        <Text>${transaction.amount}</Text>
+                    </View>
+                ))}
+            </View>
+            <Text style={defaultStyles.sectionHeader}>Widgets</Text>
+            <WidgetList />
+        </ScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  account: {
-    margin: 80,
-    alignItems: 'center',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'flex-end',
-    justifyContent: 'center',
-    gap: 10,
-  },
-  balance: {
-    fontSize: 40,
-    fontWeight: 'bold',
-  },
-  currency: {
-    fontSize: 30,
-    fontWeight: '500',
-  },
-  actionRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 20,
-  },
-  transactions: {
-    marginHorizontal: 20,
-    padding: 14,
-    backgroundColor: '#fff',
-    borderRadius: 16,
-    gap: 20,
-  },
-  circle: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: Colors.lightGray,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    account: {
+        margin: 80,
+        alignItems: 'center',
+    },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'center',
+        gap: 10,
+    },
+    balance: {
+        fontSize: 40,
+        fontWeight: 'bold',
+    },
+    currency: {
+        fontSize: 30,
+        fontWeight: '500',
+    },
+    actionRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 20,
+    },
+    transactions: {
+        marginHorizontal: 20,
+        padding: 14,
+        backgroundColor: '#5c8def',
+        borderRadius: 16,
+        gap: 20,
+    },
+    circle: {
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        backgroundColor: Colors.lightGray,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
 
 export default Page;
