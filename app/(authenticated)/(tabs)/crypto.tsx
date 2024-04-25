bdc3c741026460cf0c7513ebc5031f723e3c0625
Learn2manage <learn2manage@gmail.com>
2024-04-25 22:38:09 +0800

    Display currencies simple info on crypto.tsx

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,28 +1,48 @@
 import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { useQuery } from '@tanstack/react-query';
-//import { Currency } from '@/interfaces/crypto';
 import { Link } from 'expo-router';
 import { useHeaderHeight } from '@react-navigation/elements';
 import Colors from '@/constants/Colors';
 import { defaultStyles } from '@/constants/Styles';
 import { Ionicons } from '@expo/vector-icons';
-import { useEffect } from 'react';
+import { Currency } from '@/interfaces/crpyto';
 
 const Page = () => {
-  useEffect(() => {
-    const foo = async () => {
-      const res = await fetch('/api/listings');
-      const data = await res.json();
-      console.log('🚀 ~ foo ~ data:', data);
-    };
-    foo();
-  }, []);
+    const currencies = useQuery({
+        queryKey: ['listings'],
+        queryFn: () => fetch('/api/listings').then((res) => res.json()),
+    });
 
-  return (
-    <View style={{ paddingTop: 200 }}>
-      <Text>Crypto Page</Text>
-    </View>
-  );
+    const ids = currencies.data
+        ?.map((currency: Currency) => currency.id)
+        .join(',');
+    console.log('🚀 ~ file: crypto.tsx:17 ~ Page ~ ids:', ids);
+    const { data } = useQuery({
+        queryKey: ['info', ids],
+        queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
+        enabled: !!ids,
+    });
+
+    return (
+        <View style={{ paddingTop: 80 }}>
+            {currencies.data?.map((currency: Currency) => (
+                <View style={{ flexDirection: 'row' }} key={currency.id}>
+                    <Image
+                        source={{ uri: data?.[currency.id].logo }}
+                        style={{ width: 40, height: 40 }}
+                    />
+                    <View style={{ flex: 1, gap: 6 }}>
+                        <Text style={{ fontWeight: '600', color: Colors.dark }}>
+                            {currency.name}
+                        </Text>
+                        <Text style={{ color: Colors.gray }}>
+                            {currency.symbol}
+                        </Text>
+                    </View>
+                </View>
+            ))}
+        </View>
+    );
 };
 
 export default Page;
