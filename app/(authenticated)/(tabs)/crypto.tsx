f1e6cc709a6cbced0708e3d77c79be5a01948082
Learn2Manage <learn2manage@gmail.com>
2024-04-25 17:12:22 +0800

    Use dummy data instead of actual 3rd party API call to save quotas. Tested in crypto.tsx ok.

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { useQuery } from '@tanstack/react-query';
+//import { Currency } from '@/interfaces/crypto';
+import { Link } from 'expo-router';
+import { useHeaderHeight } from '@react-navigation/elements';
+import Colors from '@/constants/Colors';
+import { defaultStyles } from '@/constants/Styles';
+import { Ionicons } from '@expo/vector-icons';
+import { useEffect } from 'react';
 
 const Page = () => {
+  useEffect(() => {
+    const foo = async () => {
+      const res = await fetch('/api/listings');
+      const data = await res.json();
+      console.log('🚀 ~ foo ~ data:', data);
+    };
+    foo();
+  }, []);
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={{ paddingTop: 200 }}>
+      <Text>Crypto Page</Text>
     </View>
   );
 };
