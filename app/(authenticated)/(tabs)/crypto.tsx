fa99849c157da17e9de19212831180c1c58bda4e
Learn2Manage <learn2manage@gmail.com>
2024-04-26 17:25:25 +0800

    Editing crpyto.tsx display

--- app/(authenticated)/(tabs)/crypto.tsx
+++ app/(authenticated)/(tabs)/crypto.tsx
@@ -6,43 +6,52 @@ import Colors from '@/constants/Colors';
 import { defaultStyles } from '@/constants/Styles';
 import { Ionicons } from '@expo/vector-icons';
 import { Currency } from '@/interfaces/crpyto';
+import { center } from '@shopify/react-native-skia';
 
 const Page = () => {
-    const currencies = useQuery({
-        queryKey: ['listings'],
-        queryFn: () => fetch('/api/listings').then((res) => res.json()),
-    });
+  const headerHeight = useHeaderHeight();
 
-    const ids = currencies.data
-        ?.map((currency: Currency) => currency.id)
-        .join(',');
-    console.log('🚀 ~ file: crypto.tsx:17 ~ Page ~ ids:', ids);
-    const { data } = useQuery({
-        queryKey: ['info', ids],
-        queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
-        enabled: !!ids,
-    });
+  const currencies = useQuery({
+    queryKey: ['listings'],
+    queryFn: () => fetch('/api/listings').then((res) => res.json()),
+  });
 
-    return (
-        <View style={{ paddingTop: 80 }}>
-            {currencies.data?.map((currency: Currency) => (
-                <View style={{ flexDirection: 'row' }} key={currency.id}>
-                    <Image
-                        source={{ uri: data?.[currency.id].logo }}
-                        style={{ width: 40, height: 40 }}
-                    />
-                    <View style={{ flex: 1, gap: 6 }}>
-                        <Text style={{ fontWeight: '600', color: Colors.dark }}>
-                            {currency.name}
-                        </Text>
-                        <Text style={{ color: Colors.gray }}>
-                            {currency.symbol}
-                        </Text>
-                    </View>
-                </View>
-            ))}
-        </View>
-    );
+  const ids = currencies.data
+    ?.map((currency: Currency) => currency.id)
+    .join(',');
+  // console.log('🚀 ~ file: crypto.tsx:17 ~ Page ~ ids:', ids);
+  const { data } = useQuery({
+    queryKey: ['info', ids],
+    queryFn: () => fetch(`/api/info?ids=${ids}`).then((res) => res.json()),
+    enabled: !!ids,
+  });
+
+  return (
+    <ScrollView
+      style={{ backgroundColor: Colors.background }}
+      contentContainerStyle={{ paddingTop: headerHeight }}>
+      <Text style={defaultStyles.sectionHeader}>Latest Crypot</Text>
+      <View style={defaultStyles.block}>
+        {currencies.data?.map((currency: Currency) => (
+          <Link href={`/crypto/${currency.id}`} key={currency.id} asChild>
+            <TouchableOpacity
+              style={{ flexDirection: 'row', gap: 14, alignItems: 'center' }}>
+              <Image
+                source={{ uri: data?.[currency.id].logo }}
+                style={{ width: 40, height: 40 }}
+              />
+              <View style={{ flex: 1, gap: 6 }}>
+                <Text style={{ fontWeight: '600', color: Colors.dark }}>
+                  {currency.name}
+                </Text>
+                <Text style={{ color: Colors.gray }}>{currency.symbol}</Text>
+              </View>
+            </TouchableOpacity>
+          </Link>
+        ))}
+      </View>
+    </ScrollView>
+  );
 };
 
 export default Page;
